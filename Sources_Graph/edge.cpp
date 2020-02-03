dbdd53a33e54b12f84c5b16b576b0b80c1d98f5a
MathieuPietri <mathieu.pietri@etu.univ-amu.fr>
2020-02-03 17:11:42 +0100

    nettoyage partie 2

--- Sources_Graph/edge.cpp
+++
@@ -1,6 +0,0 @@
-#include "graph.h"
-
-Edge::Edge(Node *n1, Node *n2){
-    _node1 = n1;
-    _node2 = n2;
-}
--- Sources_Graph/node.cpp
+++
@@ -1,7 +0,0 @@
-#include "graph.h"
-using namespace std;
-
-Node::Node(string id, int ponderation){
-    _nameId = id;
-    _ponderation = ponderation;
-}
